ef9c4e71706f04ad5fbb9fef4fb88e1dbbcbe919
Rishu222006 <vdkaushik33@gmail.com>
2026-01-31 21:39:53 +0530

    The first prototype is ready. Code still contains safety checks.

--- back/index.js
+++ back/index.js
@@ -59,20 +59,22 @@ async function analyzeCodeWithAI(code, language) {
     ${code} `
 
 
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5:generateContent?key=${process.env.GEMINI_API_KEY}`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            contents: [
-                {
-                    role: "user",
-                    parts: [{ text: prompt }]
-                }
-            ]
-        }),
-    });
+    const response = await fetch(
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY1}`,
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                contents: [
+                    {
+                        role: "user",
+                        parts: [{ text: prompt }]
+                    }
+                ]
+            })
+        }
+    );
+
 
     const data = await response.json();
     console.log("Full AI response:", JSON.stringify(data, null, 2));
