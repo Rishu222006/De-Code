82be646ed4d5e7c7ba2b3b10dd282256c96186fd
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-02-01 09:12:19 +0000

    added changes in file.js app was not defined here

--- back/file.js
+++ back/file.js
@@ -1,6 +1,11 @@
 import multer from "multer";
 const upload = multer();
+import express from "express";
+import cors from "cors";
 
+const app = express();   // setting up express
+app.use(cors());         // setting up middlewares
+app.use(express.json());
 app.post("/analyze-file", upload.single("file"), async (req, res) => {
     const code = req.file.buffer.toString("utf-8");
     const analysis = await analyzeCodeWithAI(code);
