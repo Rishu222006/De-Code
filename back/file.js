837b72ce5939f0f7e89e855011797c43af8aa4b1
Rishu222006 <vdkaushik33@gmail.com>
2026-02-02 13:33:23 +0530

    Updated route for file.js

--- back/file.js
+++ back/file.js
@@ -1,8 +1,9 @@
 import multer from "multer";
 const upload = multer();
-import express from "express";
+import express, { Router } from "express";
 import cors from "cors";
 
+const router = express.Router();
 const app = express();   // setting up express
 app.use(cors());         // setting up middlewares
 app.use(express.json());
@@ -11,3 +12,6 @@ app.post("/analyze-file", upload.single("file"), async (req, res) => {
     const analysis = await analyzeCodeWithAI(code);
     res.json(analysis);
 });
+
+
+module.exports = router;
